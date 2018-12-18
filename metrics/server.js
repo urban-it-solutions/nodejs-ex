e10d555ee2bcc9303b3110b9cca6c89f5a33a494
Axl Foley <info@urban-its.de>
2018-12-18 15:16:49 +0100

    Update server.js

--- metrics/server.js
+++ metrics/server.js
@@ -14,3 +14,29 @@ const server = http.createServer(function(req, res) {
 server.listen(port, hostname, function() {
   console.log('Server running at http://'+ hostname + ':' + port + '/');
 });
+
+// Init
+const Prometheus = require('prom-client')
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['route'],
+  // buckets for response time from 0.1ms to 500ms
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]
+})
+
+//We need to collect the response time after each request and report it with the route label.
+
+// After each response
+httpRequestDurationMicroseconds
+  .labels(req.route.path)
+  .observe(responseTimeInMs)
+
+//We can register a route a GET /metrics endpoint to expose our metrics in the right format for Prometheus .
+
+// Metrics endpoint
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(Prometheus.register.metrics())
+})
+
