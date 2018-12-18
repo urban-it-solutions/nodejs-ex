b1536454ec4fe99e01f6d6bec4d580543db2aa86
Axl Foley <info@urban-its.de>
2018-12-18 15:27:46 +0100

    Update server.js

--- metrics/server.js
+++ metrics/server.js
@@ -1,19 +1,4 @@
-/*server.js*/
 
-const http = require('http');
-
-const hostname = '0.0.0.0';
-const port = 8080;
-
-const server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, function() {
-  console.log('Server running at http://'+ hostname + ':' + port + '/');
-});
 
 // Init
 const Prometheus = require('prom-client')
